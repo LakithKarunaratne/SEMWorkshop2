c1478c56ea4a10cead78f3a9251a25a6a2826e4c
Lakith <lakith.hos@outlook.com>
2017-09-26 02:14:23 +0800

    Post event updates
    Added comments

--- Live Coding/25-09-2017/API_Calls.py
+++ Live Coding/25-09-2017/API_Calls.py
@@ -1,14 +1,21 @@
-# this is on Google API
+# this is based on Google Cloud, you will need to use Drive API and setup a Key first
 
-import gspread
+# by defaults you won't have any of these files on your PC, let PyCharm install them for you,
+# secondary option is to use 'python pip' you can read more about it on the documentation
+
+import gspread # from : https://github.com/burnash/gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://spreadsheets.google.com/feeds']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('jasonfile.json',scope)
+scope = ['https://spreadsheets.google.com/feeds'] # this is where we fetch data from, all data presented here isn't human readable
 
-client = gspread.authorize(credentials)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('jasonfile.json',scope) # set the scope and the jason file you generated from google cloud
 
-sheet = client.open("TestSheet").sheet1
+client = gspread.authorize(credentials) # use the above credentials to authorise a client through gspread
 
-content = sheet.get_all_records()
-print(content)
+sheet = client.open("TestSheet").sheet1 # get data from the created google sheet
+
+content = sheet.get_all_records() # fetch all the data in the sheet
+
+print(content) # print all the fetched data
+
+# we have not covered update cells part,refer to the git link provided upon import
